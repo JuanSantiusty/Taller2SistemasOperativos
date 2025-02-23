e88b56d9fd747457a6a3ebbaca9810311cea5160
juan1santiusty <juan@Debian.Juan>
2025-02-23 18:45:26 -0500

    Estructura de menu

---
+++ main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main(){
+  int opc=0;
+
+  do{
+    printf("\nElija una de las opciones");
+    printf("\n1- Recopilar la informacion de la empresa");
+    printf("\n2- Solicitar y procesar las fcaturas del mes de un semestre");
+    printf("\n3- Carcular el recaudo obtenido por cada mes");
+    printf("\n4- Calcular y mostrar el recaudo obtenido del semestre, basado en el recaudo obtenido por cada mes");
+    printf("\n5- Salir");
+    scanf("%d",&opc);
+    switch(opc){
+    case 1{
+      system("cls");
+      getche();
+    }
+    
+    case 2{
+      system("cls");
+      getche();
+    }
+    
+    case 3{
+      system("cls");
+      getche();
+    }
+    
+    case 4{
+      system("cls");
+      getche();
+    }
+    
+   
+    
+  }while(opc!=5);
+  return 0;
+}
